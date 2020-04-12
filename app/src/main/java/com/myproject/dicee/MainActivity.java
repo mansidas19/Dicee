d99cc98aa4ac1b667b2f9c89018dd7f6854a6e35
mansi <mansidas19@rediffmail.com>
2020-04-12 15:08:55 +0530

    changes

--- app/src/main/java/com/myproject/dicee/MainActivity.java
+++ app/src/main/java/com/myproject/dicee/MainActivity.java
@@ -18,12 +18,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button rollButton;
+        Button button4;
         rollButton = (Button) findViewById(R.id.rollButton);
+        button4 = (Button) findViewById(R.id.button4);
 
-        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
 
-        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
 
+        final int[]  diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Dicee", "BUTTON PRESSED");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee","the random no. is"+number);
+
+                leftDice.setImageResource(diceArray[number]);
+
+            }
+        });
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
                 int number = randomNumberGenerator.nextInt(6);
 
                 Log.d("Dicee","the random no. is"+number);
+
+                rightDice.setImageResource(diceArray[number]);
             }
         });
 
