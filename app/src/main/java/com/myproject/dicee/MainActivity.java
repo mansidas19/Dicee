ed39360f82caff8c8e4f5d6785ce450ae6625a90
mansi <mansidas19@rediffmail.com>
2020-04-12 13:02:44 +0530

    random no.s

--- app/src/main/java/com/myproject/dicee/MainActivity.java
+++ app/src/main/java/com/myproject/dicee/MainActivity.java
@@ -3,6 +3,12 @@ package com.myproject.dicee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Dicee", "BUTTON PRESSED");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee","the random no. is"+number);
+            }
+        });
+
+        }
     }
-}
