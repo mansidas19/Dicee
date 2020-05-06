c9fd84cc8e5daa7dd85c101518828d6a8b3ad97b
mansi <mansidas19@rediffmail.com>
2020-05-06 16:14:53 +0530

    constraint

--- app/src/main/java/com/myproject/dicee/MainActivity.java
+++ app/src/main/java/com/myproject/dicee/MainActivity.java
@@ -2,6 +2,10 @@ package com.myproject.dicee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,15 +16,29 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    SoundPool mSoundPool;
+    private int mLeftSoundId;
+    private int mRightSoundId;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSoundPool = new SoundPool(7,AudioManager.STREAM_MUSIC,0);
+        mLeftSoundId = mSoundPool.load(this,R.raw.click, 1);
+        mRightSoundId = mSoundPool.load(this, R.raw.click, 1);
         Button rollButton;
         Button button4;
         rollButton = (Button) findViewById(R.id.rollButton);
         button4 = (Button) findViewById(R.id.button4);
+
 
         final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
 
@@ -39,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+
                 Log.d("Dicee", "BUTTON PRESSED");
 
                 Random randomNumberGenerator = new Random();
@@ -48,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Dicee","the random no. is"+number);
 
                 leftDice.setImageResource(diceArray[number]);
+
+                mLeftSoundId=mSoundPool.play(mLeftSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
 
             }
         });
@@ -64,8 +85,18 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Dicee","the random no. is"+number);
 
                 rightDice.setImageResource(diceArray[number]);
+
+                mRightSoundId=mSoundPool.play(mRightSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+
             }
         });
-
         }
+
+    public void playleft(View view) {mSoundPool.play(mLeftSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
     }
+
+    public void playright(View view) {mSoundPool.play(mRightSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+}
+
+
